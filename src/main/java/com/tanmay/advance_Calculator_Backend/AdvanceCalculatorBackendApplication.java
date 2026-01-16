8473ce113883429500f89a2b1c94e7915d2f965d
Tanmay-Anand <tanmayanand2002@gmail.com>
2026-01-16 23:46:26 +0530

    Initial Commit

---
+++ src/main/java/com/tanmay/advance_Calculator_Backend/AdvanceCalculatorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tanmay.advance_Calculator_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvanceCalculatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvanceCalculatorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanmay/advance_Calculator_Backend/AdvanceCalculatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanmay.advance_Calculator_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvanceCalculatorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
