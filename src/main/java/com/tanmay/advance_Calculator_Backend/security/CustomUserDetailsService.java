b2a465947b4aba20531308ce8395236dde980068
Tanmay-Anand <tanmayanand2002@gmail.com>
2026-01-18 18:09:30 +0530

    Added security configurations along with setting up JWT Authentication Filter.

--- src/main/java/com/tanmay/advance_Calculator_Backend/security/CustomUserDetailsService.java
+++ src/main/java/com/tanmay/advance_Calculator_Backend/security/CustomUserDetailsService.java
@@ -1,4 +1,28 @@
 package com.tanmay.advance_Calculator_Backend.security;
 
-public class CustomUserDetailsService {
+import com.tanmay.advance_Calculator_Backend.domain.User;
+import com.tanmay.advance_Calculator_Backend.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepo;
+
+    public CustomUserDetailsService(UserRepository userRepo) { this.userRepo = userRepo; }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepo.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .authorities("USER")
+                .build();
+    }
 }
--- src/main/java/com/tanmay/advance_Calculator_Backend/security/JwtAuthFilter.java
+++ src/main/java/com/tanmay/advance_Calculator_Backend/security/JwtAuthFilter.java
@@ -1,4 +1,67 @@
 package com.tanmay.advance_Calculator_Backend.security;
 
-public class JwtAuthFilter {
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtUtils jwtUtils;
+    private final CustomUserDetailsService userDetailsService;
+
+    public JwtAuthFilter(JwtUtils jwtUtils, CustomUserDetailsService uds) {
+        this.jwtUtils = jwtUtils;
+        this.userDetailsService = uds;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req,
+                                    HttpServletResponse res,
+                                    FilterChain chain) throws ServletException, IOException {
+        String path = req.getRequestURI();
+        if(path.startsWith("/api/auth/")) {
+            chain.doFilter(req, res);
+            return;
+        }
+
+        String header= req.getHeader("Authorization");
+        if (header != null && header.startsWith("Bearer ")) {
+            String token = header.substring(7);
+
+            try {
+                if (jwtUtils.validateToken(token)) {
+
+                    Claims claims = jwtUtils.getClaims(token);
+                    String username = claims.getSubject();
+
+                    UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+                    UsernamePasswordAuthenticationToken auth =
+                            new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                    auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(req));
+
+                    SecurityContextHolder.getContext().setAuthentication(auth);
+                }
+            } catch (JwtException ex) {
+
+            }
+        }
+        chain.doFilter(req, res);
+    }
 }
